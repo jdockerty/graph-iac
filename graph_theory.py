ae0789c1bda577dbff592307147953189b907789
Jack Dockerty <jdockerty19@gmail.com>
2019-10-17 21:06:50 +0100

    Working  on dictionary node traversal.

--- graph_theory.py
+++ graph_theory.py
@@ -7,6 +7,7 @@ from pathlib import Path
 class graph_structure:
     path_to_file = ""
     current_graph = None
+    node_dependencies = {}
 
     def __init__(self):
         self.current_graph = nx.Graph()
@@ -14,20 +15,42 @@ class graph_structure:
     def get_json_to_dict(self):
         file_path = Path(self.path_to_file)
         with file_path.open(mode='r') as my_file:
-            JSON_to_dict = dict(json.load(my_file))
-            return JSON_to_dict
+            full_json_to_dict = dict(json.load(my_file))
+            return full_json_to_dict
 
     def set_filepath(self, filepath):
         self.path_to_file = filepath
 
-    def get_resources(self):
-        json_as_dict = self.get_json_to_dict()
-        return json_as_dict['Resources'].keys()
+    def get_resources_count(self):
+        resources_json_as_dict = self.get_json_to_dict()
+        return resources_json_as_dict['Resources'].keys()
 
     def add_nodes(self):
-        self.current_graph.add_node(self.get_resources())
+        self.current_graph.add_node(self.get_resources_count())
 
+    def map_edges_to_nodes(self):
+        current_JSON = self.get_json_to_dict()['Resources'] # Pulls the full JSON, BUT ONLY RESOURCES KEY AND AFTER
+        print(current_JSON)
+        #counter = 0
+        resources_list = list(current_JSON)
+        print(resources_list)
+        print("\n\n\n ----DICT----")
+
+        for key in list(current_JSON.items()):
+            sub_dict = key[1]
+            print(sub_dict)
+            try:
+                if sub_dict['Ref'] or sub_dict['InstanceId'] or sub_dict['DependsOn']:
+                    self.node_dependencies[key] = sub_dict['']
+                # IF SUCCESSFUL THEN ADD THAT VALUE INTO THE DEPENDENCY
+            except KeyError:
+                print("Skip")
+                continue
 
 test = graph_structure()
 test.set_filepath(r'C:\Users\Jack\PycharmProjects\GraphTheoryToIaC\Files_To_Read\template.json')
-print(test.get_resources())
+test.map_edges_to_nodes()
+
+
+# IDEA - TRAVERSE RESOURCES SUB-DICT AND READ FOR DependsOn/Other dependencies, if exists then can add to dict. Map edges
+# from that generated dict.
